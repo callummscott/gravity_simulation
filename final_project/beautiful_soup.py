27c18fa633af68a7a1079854a27ee998ceef9377
callummscott <134177091+callummscott@users.noreply.github.com>
2024-08-13 23:55:50 +0100

    v1.0.3

--- final_project/beautiful_soup.py
+++ final_project/beautiful_soup.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup as BS
 import urllib.request as libreq
 import urllib.parse as libparse
+import tldextract
+import json
 import re
 
 
@@ -8,30 +10,90 @@ CHARLIMIT = 10_000_000
 AGENCIESPATH = "C:\\Github\\Learning\\final_project\\agencies\\"
 
 
-class Pattern:
-        """
-        Primarily stores useful Regex patterns 
-        """
-        # Searches for 'propertyCards'
-        p1 = r"propertyCard-(\w+)\W" 
-        # Searches for 'propertyCard'-related image urls
-        p2 = r"<img alt=\"Property Image \d?\d\" itemprop=\"image\" src=\"(.+)\""
-        # Captures a relevant 'path' for a URL 
-        p3 = r"<a class=\"propertyCard-additionalImgs\" href=\"(.+?)\""
-        # UK URL pattern, captures: 1) 'Origin' of url [Ignores last '/'], 2) Domain of URL 
-        p4 = r"(https:\/\/(?:www.)?(.+?)(?:(?:\.co|\.org).uk|\.com))\/"
-        # urllib.request headings -- a.k.a. (some) class attributes and methods
-        p5 = r"title=\"urllib\.request\.(.*?)\""
+class JSONLoadError(Exception):
+    """Custom exception for JSON loading errors."""
+    pass
 
-class Webpage:
+
+class Pattern:
+    """
+    Primarily stores useful Regex patterns
+    """
+
+    # Searches for 'propertyCards'
+    p1 = r"propertyCard-(\w+)\W"
+    # Searches for 'propertyCard'-related image urls
+    p2 = r"<img alt=\"Property Image \d?\d\" itemprop=\"image\" src=\"(.+)\""
+    # Captures a relevant 'path' for a URL
+    p3 = r"<a class=\"propertyCard-additionalImgs\" href=\"(.+?)\""
+    # UK URL pattern, captures: 1) 'Origin' of url [Ignores last '/'], 2) Domain of URL
+    p4 = r"(https:\/\/(?:www.)?(.+?)(?:(?:\.co|\.org).uk|\.com))\/"
+    # urllib.request headings -- a.k.a. (some) class attributes and methods
+    p5 = r"title=\"urllib\.request\.(.*?)\""
+
+
+class HTML:
     def __init__(self):
-        # 'Origin' attribute
-        # 'TLD' attribute
-        # 'Path' attribute
-        # Query formatting as func of domain name
-        #   i.e. rightmovepage.qprotocol(user values) -> rightmovepath / full rightmove url  
-        #     or just handle qprotocol separately, considering it'll just be domain dependent.
         ...
+
+class URL:
+    def __init__(self):
+        super().__init__()
+
+        self.full = ...           #'Origin' attribute
+        self.tld = ...              # 'TLD' attribute
+        self.path = ...             # 'Path' attribute
+        self.query_protocol = ...   # Query formatting as func of domain name
+                                    #   i.e. rightmovepage.qprotocol(user values) -> rightmovepath / full rightmove url
+                                    #     or just handle qprotocol separately, considering it'll just be domain dependent.
+        ...
+    
+    def to_pretty_txt(self, dest: str) -> None:
+        ...
+
+    def is_in_json(self):
+        ...
+
+
+class LettingAgency:
+    def __init__(self, name): 
+        self.name = name  # 'chappandmatt' <- "chappellandmatthews"
+        if not self.__validate_name():
+            raise ValueError("Invalid agency name")
+
+        self.domain = self.__get_domain()  # "rightmove"
+        self.query_tags = self.__get_query_tags()  # [viewType, keywords, ...]
+        self.url_base = ...  # "https://www.rightmove.co.uk/property-to-rent/find.html?"
+        self.data = self.__load_agency_data()
+
+    def __validate_name(self):
+        return self.name in self.load_full_json()
+
+    def __load_agency_data(self):
+        if (agency_data := self.load_full_json()[self.name]) is None:
+            print("Failed to load agency data")
+            return None
+        return agency_data
+
+    def __get_domain(self):
+        return self.__load_agency_data()["domain"]
+
+    def __get_query_tags(self):
+        return self.__load_agency_data()["query tags"]
+
+    def load_full_json(self):
+        try:
+            with open("Agencies.JSON", "r") as file:
+                return json.load(file)
+        except json.decoder.JSONDecodeError:
+            # Expect two errors: one from this and one from `self.domain.__get_domain()`'s (None)['domain'] indexing
+            raise JSONLoadError
+
+    def WIP_change_name(self, new_name):
+        try:
+            LettingAgency(new_name).domain
+        except ValueError:
+            self.name = self.name
 
 
 
@@ -42,16 +104,18 @@ def url_in_list(input_url: str) -> bool:
     Change these variable names!
     """
     if not input_url:
-        raise ValueError("url_in_list: No input entered")
-    
-    # Matches origin of input_url, if valid
+        raise ValueError("No input entered")
+
     if search := re.search(Pattern.p4, input_url):
         input_origin = search.group(1)
     else:
-        raise ValueError("url_in_list: Input URL does not fit expected pattern")
-    
-    # Check if origin of URLs in each row of agencies.txt matches input_origin 
-    with open(AGENCIESPATH + "agencies.txt", 'r') as file:
+        if search := re.search(Pattern.p4[:-2], input_url):
+            raise ValueError("Missing '/' at end of input URL`")
+        else:
+            raise ValueError("Input URL does not fit expected pattern")
+
+    # Check if origin of URLs in each row of agencies.txt matches input_origin
+    with open(AGENCIESPATH + "agencies.txt", "r") as file:
         for row in file:
             if row_origin := re.search(Pattern.p4, row).group(1) == input_origin:
                 return True
@@ -67,17 +131,9 @@ def url_to_txt(url: str, dest:str) -> None:
             file.write(row)
 
 
-def parse_file_to_BS(filename: str) -> BS:
-    """
-    Takes in a filename in 'filename.html' format
-    Outputs the BS (BeautifulSoup) object, parsed in 'html'
-        Read-only, encoded in utf-8
-    """
-    with open(filename, 'r', encoding='utf-8') as file:
-        return BS(file, 'html.parser')
-    
-
-def matches_to_txt(input_file: str, output_file: str, pattern: str, *catches: list) -> None:
+def matches_to_txt(
+    input_file: str, output_file: str, pattern: str, *catches: list
+) -> None:
     """
     Searches for `pattern` matches in `input_file`, outputs matches to `output_file`
     Want to add functionality for which 'groups' of the match object to capture
@@ -86,9 +142,9 @@ def matches_to_txt(input_file: str, output_file: str, pattern: str, *catches: li
         attr_pattern = getattr(Pattern, pattern)
     except AttributeError:
         raise AttributeError("Invalid pattern choice")
-    
+
     matches = []
-    with open(input_file, 'r') as file:
+    with open(input_file, "r") as file:
         for line in file:
             matches.append(re.search(attr_pattern, line))
     list_matches = []
@@ -96,37 +152,63 @@ def matches_to_txt(input_file: str, output_file: str, pattern: str, *catches: li
     for match in matches:
         if match and match.group(1) not in list_matches:
             list_matches.append(match.group(1))
-    with open(output_file, 'w') as newfile:
+    with open(output_file, "w") as newfile:
         for match in list_matches:
-            newfile.write(match + '\n')
+            newfile.write(match + "\n")
 
 
-def rightmove_query_parser():
+def parse_file_to_BS(filename: str) -> BS:
     """
-    Takes info from rightmove/sample_urls.txt and makes dictionary of unique query terms
+    Takes in a filename in 'filename.html' format
+    Outputs the BS (BeautifulSoup) object, parsed in 'html'
+        Read-only, encoded in utf-8
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        return BS(file, "html.parser")
+
+
+def WIP_rightmove_query_parser():
+    """
+    ### WIP ###
+    Takes info from 'rightmove/sample_urls.txt' and makes dictionary of unique query terms
 
     Could strip the information before the query from the URL to save storage but cba with that yet
+    Could also NOT rewrite the ENTIRE file every time it runs, but oh well -- for now.
     """
-    unique_qs = {}
-    with open(AGENCIESPATH+'rightmove/sample_urls.txt', 'r') as file:
-        for row in file:
-            url_queries_string = libparse.urlparse(row)[4]
-            query_dict = libparse.parse_qs(url_queries_string)
-            for query in query_dict:
-                if query in unique_qs:
-                    for item in query_dict[query]:
-                        if item not in unique_qs[query]:
-                            unique_qs[query].append(item)
-                else:
-                    unique_qs[query] = query_dict[query]
+    query_tags_element = 4
+    unique_tags_values = dict()
 
-    with open(AGENCIESPATH+'rightmove/query_terms.txt', 'w') as file:
-        for index, query in enumerate(unique_qs, start=1):
-            file.write(f"#{index}: {query} = {unique_qs[query]}\n" )
+    with open(AGENCIESPATH + "rightmove/sample_urls.txt", "r") as file:
+
+        for line_num, row in enumerate(file, start=1):
+            if row.isspace() or row.lstrip()[0] == "#":
+                continue
+
+            # Isolates individual URL's query string:
+            #   e.g. 'locationIdentifier=REGION%5E6574&maxBedrooms=3&maxPrice=1750..."
+            url_query_tags_string = libparse.urlparse(row)[query_tags_element]
+
+            # Converts query string to dictionary with tags as keys,
+            #   and their values as... values, stored in a list.
+            tag_value_dict = libparse.parse_qs(url_query_tags_string)
+
+            ### FIX ### This has got to be able to be simplified or made quicker somehow
+            for tag in tag_value_dict.keys():
+                if tag in unique_tags_values.keys():  # .keys is redundant but clearer
+                    for value in tag_value_dict[tag]:
+                        if value not in unique_tags_values[tag]:
+                            unique_tags_values[tag].append(value)
+                else:
+                    unique_tags_values[tag] = tag_value_dict[tag]
+
+    with open(AGENCIESPATH + "rightmove/query_terms.txt", "w") as file:
+        for index, tag in enumerate(unique_tags_values, start=1):
+            file.write(f"#{index}: {tag} = {unique_tags_values[tag]}\n")
 
 
 def main():
-    rightmove_query_parser()
+    ...
+
 
 if __name__ == "__main__":
     main()
